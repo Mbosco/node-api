df7399f4a98f29e1451da24ff3a2b89c55f2b935
Bosco Mutunga <boscomutunga@gmail.com>
2014-10-20 20:10:38 +0300

    complete crud api

--- server.js
+++ server.js
@@ -24,13 +24,67 @@ router.use(function(req, res, next){
     console.log('request received, something is happening');
     next();//make sure we proceed
 });
+
 router.get('/', function(req, res){
     res.json(
 	{message: 'hooray! welcome to our api!'}
     );
 });
 // more routes for our api happen here
+
+router.route('/bears')
+    .post(function(req, res){
+	     var bear = new Bear();
+	      bear.name = req.body.name;
+	       //save the bear and check for errors
+	        bear.save(function(err){
+	           if(err)
+		             res.send(err);
+	               res.json({message: 'bear created'});
+	       });
+    })
+    .get(function(req, res){
+	       Bear.find(function(err, bears){
+	          if(err)
+		            res.send(err);
+	          res.json(bears);
+	      });
+    });
+
+router.route('/bears/:bear_id')
+    // get the bear with that id
+    .get(function(req, res){
+	       Bear.findById(req.params.bear_id, function(err, bear){
+	          if(err)
+		            res.send(err);
+	          res.json(bear);
+	       });
+    })
+    // update the bear with this id
+    .put(function(req, res){
+	       Bear.findById(req.params.bear_id, function(err, bear){
+	          if(err)
+		            res.send(err);
+	          bear.name = req.body.name //update the bears info
+	          //save the bear
+	          bear.save(function(err){
+		            if(err)
+		                res.send(err);
+		            res.json({message: 'Bear updated!'});
+	          });
+	      });
+    })
+    .delete(function(req, res){
+      Bear.remove({
+        _id: req.params.bear_id
+      }, function(err, bear){
+        if(err)
+          res.send(err);
+        res.json({message: 'Successfully deleted'});
+      });
+    });
+
 app.use('/api', router);
 
 app.listen(port);
-console.log('Magic happens on port' + port);
+console.log('listening on port ' + port);
